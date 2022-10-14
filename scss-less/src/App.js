4a9a66fc0d1f9aa69db1b6ce7c45ebfe8e27e29c
Reaverimg <phieulinh341@gmail.com>
2022-10-14 10:43:12 +0700

    first commit

---
+++ scss-less/src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+import Cards from "./components/Cards";
+function App() {
+  return (
+    <div className="App">
+      <Cards></Cards>
+    </div>
+  );
+}
+
+export default App;
